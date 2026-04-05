7f935e0c79749278cde82544a3c4c298a29ebca2
Zac Owen <connect@mylegnd.com>
2026-04-05 00:46:13 -0700

    your message

--- AgentPortal/Controllers/API/FinanceToolStatesController.cs
+++ AgentPortal/Controllers/API/FinanceToolStatesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using AgentPortal.Services;
+using System.Text.Json.Nodes;
 
 namespace AgentPortal.Controllers.API
 {
