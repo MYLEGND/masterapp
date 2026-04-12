314490d7b52e10e4bc2b923420c272cab1175706
Zac Owen <connect@mylegnd.com>
2026-04-11 23:40:08 -0700

    WIP: 2026-04-11 23:40

--- Protect-Website/Controllers/HomeController.cs
+++ Protect-Website/Controllers/HomeController.cs
@@ -8,5 +8,19 @@ namespace ProtectWebsite.Web.Controllers
         {
             return View(); // Views/Home/Index.cshtml
         }
+
+        [HttpGet("Privacy")]
+        [HttpGet("Home/Privacy")]
+        public IActionResult Privacy()
+        {
+            return View(); // Views/Home/Privacy.cshtml
+        }
+
+        [HttpGet("Terms")]
+        [HttpGet("Home/Terms")]
+        public IActionResult Terms()
+        {
+            return View(); // Views/Home/Terms.cshtml
+        }
     }
 }
