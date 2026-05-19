dcebf1dc80ef14441392ce73deaf67079d3390d0
MYLEGND <zac.owen@mylegnd.com>
2026-05-19 11:15:16 -0700

    WIP: 2026-05-19 11:15

---
+++ Protect-Website/Models/LifeEstimateResult.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace Protect_Website.Models
+{
+    public sealed class LifeEstimateResult
+    {
+        public string PolicyKey { get; set; } = "";
+        public string PolicyType { get; set; } = "";
+        public int CoverageAmount { get; set; }
+        public decimal EstimatedLowMonthly { get; set; }
+        public decimal EstimatedHighMonthly { get; set; }
+        public string RecommendationReason { get; set; } = "";
+        public string Disclaimer { get; set; } = "";
+        public IReadOnlyList<string> Reasons { get; set; } = new List<string>();
+    }
+
+    public sealed class LifeEstimatePreviewResponse
+    {
+        public LifeEstimateResult Primary { get; set; } = new();
+        public LifeEstimateResult Secondary { get; set; } = new();
+        public string AgeBand { get; set; } = "";
+        public string TobaccoUse { get; set; } = "";
+        public string CoverageGoal { get; set; } = "";
+        public string ProtectingWho { get; set; } = "";
+        public string HealthAssumption { get; set; } = "Average Health";
+        public string Disclaimer { get; set; } = "";
+    }
+}
