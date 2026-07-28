f548d77305c045d831d647783fbe980048e6c471
MYLEGND <zac.owen@mylegnd.com>
2026-07-27 18:25:43 -0700

    Default financial presentation response for compatibility

--- Legend-ios/Legend/Features/Home/MobileHomeModels.swift
+++ Legend-ios/Legend/Features/Home/MobileHomeModels.swift
@@ -119,7 +119,7 @@ struct MobileFinancialSnapshotResponse: Codable, Equatable, Sendable {
     let intelligence: MobileFinancialIntelligenceSummary?
     let upcomingBills: [MobileUpcomingBill]
     let operatingSystem: MobileFinancialOperatingSystemSnapshotResponse?
-    let presentation: MobileFinancialPresentationResponse?
+    let presentation: MobileFinancialPresentationResponse? = nil
 }
 
 struct MobileFinancialPresentationResponse: Codable, Equatable, Sendable {
