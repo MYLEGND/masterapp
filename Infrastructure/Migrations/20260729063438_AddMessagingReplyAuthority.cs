a9f42634616b850372e3dfad2f93ee46e6aad247
MYLEGND <zac.owen@mylegnd.com>
2026-07-28 23:38:22 -0700

    Add messaging reply authority migration

---
+++ Infrastructure/Migrations/20260729063438_AddMessagingReplyAuthority.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessagingReplyAuthority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReplyToMessageId",
+                table: "InternalMessages",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InternalMessages_ReplyToMessageId",
+                table: "InternalMessages",
+                column: "ReplyToMessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InternalMessages_InternalMessages_ReplyToMessageId",
+                table: "InternalMessages",
+                column: "ReplyToMessageId",
+                principalTable: "InternalMessages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InternalMessages_InternalMessages_ReplyToMessageId",
+                table: "InternalMessages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InternalMessages_ReplyToMessageId",
+                table: "InternalMessages");
+
+            migrationBuilder.DropColumn(
+                name: "ReplyToMessageId",
+                table: "InternalMessages");
+        }
+    }
+}
