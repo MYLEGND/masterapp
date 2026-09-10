1ba2ab37eef80b18574f293d4eadf84fa18d81c6
MYLEGND <zac.owen@mylegnd.com>
2026-09-10 06:31:22 -0700

    Advance Android version code to 6 for the replacement Play upload

--- Legend-Android/app/build.gradle.kts
+++ Legend-Android/app/build.gradle.kts
@@ -163,7 +163,7 @@ android {
         applicationId = legendApplicationId
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
