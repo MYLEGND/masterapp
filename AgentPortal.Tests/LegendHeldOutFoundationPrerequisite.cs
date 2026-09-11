909acc2c2238d8b04e5c69f83c3da9063d5b073b
MYLEGND <zac.owen@mylegnd.com>
2026-09-10 19:42:43 -0700

    Import governed language identity in foundation prerequisite audit

--- AgentPortal.Tests/LegendHeldOutFoundationPrerequisite.cs
+++ AgentPortal.Tests/LegendHeldOutFoundationPrerequisite.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Messaging;
 using Infrastructure.Data;
 using Infrastructure.Messaging;
 using Microsoft.EntityFrameworkCore;
