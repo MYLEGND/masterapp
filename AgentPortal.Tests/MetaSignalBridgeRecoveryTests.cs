466a28ec76f74610a86ff1e47d1fa62517d33cf6
MYLEGND <zac.owen@mylegnd.com>
2026-09-25 12:20:19 -0700

    Repoint AgentPortal.Tests/MetaSignalBridgeRecoveryTests.cs to centralized Meta authority

--- AgentPortal.Tests/MetaSignalBridgeRecoveryTests.cs
+++ AgentPortal.Tests/MetaSignalBridgeRecoveryTests.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
-using ProtectWebsite.Services.MetaSignal;
+using Infrastructure.Analytics;
 using Xunit;
 
 namespace AgentPortal.Tests;
