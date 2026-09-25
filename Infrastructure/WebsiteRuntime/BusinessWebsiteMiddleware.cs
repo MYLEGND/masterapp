659f67092e8ec5bcb5d2d416da3dd9e17bc3bf34
MYLEGND <zac.owen@mylegnd.com>
2026-09-25 12:43:34 -0700

    Add explicit shared framework imports for centralized website authority

--- Infrastructure/WebsiteRuntime/BusinessWebsiteMiddleware.cs
+++ Infrastructure/WebsiteRuntime/BusinessWebsiteMiddleware.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System.Net;
 using System.Text.Json;
 using System.Xml.Linq;
