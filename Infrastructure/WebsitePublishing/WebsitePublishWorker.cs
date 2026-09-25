6e8de7e98564382e33c024adce18c907a8c5f01f
MYLEGND <zac.owen@mylegnd.com>
2026-09-25 12:46:53 -0700

    Complete explicit framework imports for centralized website authority

--- Infrastructure/WebsitePublishing/WebsitePublishWorker.cs
+++ Infrastructure/WebsitePublishing/WebsitePublishWorker.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
